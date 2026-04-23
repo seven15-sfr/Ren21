f6e777df1c81fa7281f513a73c0b016a0c4b2e80
seven15-sfr <re23mh1@gmail.com>
2026-04-22 22:11:24 -0600

    Add files via upload

---
+++ nodos.c
@@ -0,0 +1,105 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Dato {
+    int d;
+    struct Dato *Ptisig; 
+};
+
+int menu(void) {
+    int op;
+    printf("\n--- MENU ---\n");
+    printf("1. Crear dato (Insertar al final)\n");
+    printf("2. Mostrar todos los datos\n");
+    printf("3. Eliminar ultimo campo\n");
+    printf("4. Salir: ");
+    scanf("%d", &op);
+    return op;
+}
+
+struct Dato* crearDato() {
+    struct Dato *Ptrtemp = (struct Dato*)malloc(sizeof(struct Dato));
+    if (Ptrtemp == NULL) {
+        return NULL;
+    }
+    printf("Ingrese el valor entero: ");
+    scanf("%d", &Ptrtemp->d);
+    Ptrtemp->Ptisig = NULL;
+    printf("Modulo creado en memoria.\n");
+    return Ptrtemp;
+}
+
+int main(void) {
+    int op;
+    struct Dato *Ptr = NULL;    
+    struct Dato *Ptraux = NULL;  
+    struct Dato *Ptrtemp = NULL; 
+
+    do {
+        op = menu();
+        switch(op) {
+            case 1:
+                Ptrtemp = crearDato();
+                if (Ptrtemp == NULL) {
+                    printf("Error: No se reservo memoria\n");
+                } else {
+                    if (Ptr == NULL) {
+                        Ptr = Ptrtemp;
+                    } else {
+                        Ptraux = Ptr;
+                        while (Ptraux->Ptisig != NULL) {
+                            Ptraux = Ptraux->Ptisig;
+                        }
+                        Ptraux->Ptisig = Ptrtemp;
+                    }
+                }
+                break;
+
+            case 2:
+                if (Ptr == NULL) {
+                    printf("La lista esta vacia.\n");
+                } else {
+                    Ptraux = Ptr;
+                    printf("Contenido: ");
+                    while (Ptraux != NULL) {
+                        printf("[%d] -> ", Ptraux->d);
+                        Ptraux = Ptraux->Ptisig;
+                    }
+                    printf("NULL\n");
+                }
+                break;
+
+            case 3: 
+                if (Ptr == NULL) {
+                    printf("Nada que eliminar.\n");
+                } else if (Ptr->Ptisig == NULL) {
+                    free(Ptr);
+                    Ptr = NULL;
+                    printf("Lista ahora vacia.\n");
+                } else {
+                    Ptraux = Ptr;
+                    while (Ptraux->Ptisig->Ptisig != NULL) {
+                        Ptraux = Ptraux->Ptisig;
+                    }
+                    free(Ptraux->Ptisig);
+                    Ptraux->Ptisig = NULL;
+                    printf("Ultimo nodo eliminado.\n");
+                }
+                break;
+
+            case 4:
+                while (Ptr != NULL) {
+                    Ptraux = Ptr;
+                    Ptr = Ptr->Ptisig;
+                    free(Ptraux);
+                }
+                printf("Saliendo...\n");
+                break;
+
+            default:
+                printf("Opcion no valida\n");
+        }
+    } while(op != 4);
+
+    return 0;
+}
